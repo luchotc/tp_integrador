5e5d6e958b7696043095bdee55a0eba40ba74d0f
Lucho Cannavo <luchotc@hotmail.com.ar>
2015-04-28 16:10:16 -0300

    agregando usuario

--- src/main/java/ar/com/ddsutn/integrador/Usuario.java
+++ src/main/java/ar/com/ddsutn/integrador/Usuario.java
@@ -2,4 +2,17 @@ package ar.com.ddsutn.integrador;
 
 public class Usuario {
 
+	private Double altura;
+	private Double peso;
+	
+	public Usuario (Double altura, Double peso){
+	this.altura = altura;
+	this.peso = peso;
+	}
+	
+	public Double imc ()
+	 {
+	  Double cuadrado = Math.pow(altura, 2);
+	  return peso/cuadrado;
+	 }
 }
