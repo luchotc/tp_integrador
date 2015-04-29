ec8a8f614efb96c5b832cf9a5198c6b3015c6391
NicooRama <nicolaserama@gmail.com>
2015-04-29 20:57:32 -0300

    atributos nuevos

--- src/main/java/ar/com/ddsutn/integrador/Usuario.java
+++ src/main/java/ar/com/ddsutn/integrador/Usuario.java
@@ -1,16 +1,30 @@
 package ar.com.ddsutn.integrador;
 
+import java.time.LocalDate;
+import java.util.Collection;
+
 public class Usuario {
 
+	private String nombre;
+	private String sexo;
+	private LocalDate fechaNacimiento;
+	private Collection <String> preferenciasAlimenticias;
+	private Collection <String> palabrasDisgustan;
+	
 	private Double altura;
 	private Double peso;
 	
 	public Usuario()
 	{super();}
 	
-	public Usuario (Double altura, Double peso){
-	this.altura = altura;
-	this.peso = peso;
+	public Usuario (String nombre,String sexo, LocalDate fechaNacimiento, Double altura, Double peso,Collection<String> preferenciasAlimentacias,Collection<String>palabrasDisgustan){
+		this.nombre = nombre;
+		this.sexo = sexo;
+		this.fechaNacimiento = fechaNacimiento;
+		this.altura = altura;
+		this.peso = peso;
+		this.preferenciasAlimenticias = preferenciasAlimentacias;
+		this.palabrasDisgustan = palabrasDisgustan;
 	}
 	
 	public Double imc ()
@@ -18,4 +32,20 @@ public class Usuario {
 	  Double cuadradoAltura = Math.pow(altura, 2);
 	  return peso/cuadradoAltura;
 	 }
+
+	public Collection <String> getPreferenciasAlimenticias() {
+		return preferenciasAlimenticias;
+	}
+
+	public void setPreferenciasAlimenticias(Collection <String> preferenciasAlimenticias) {
+		this.preferenciasAlimenticias = preferenciasAlimenticias;
+	}
+
+	public Collection <String> getPalabrasDisgustan() {
+		return palabrasDisgustan;
+	}
+
+	public void setPalabrasDisgustan(Collection <String> palabrasDisgustan) {
+		this.palabrasDisgustan = palabrasDisgustan;
+	}
 }
