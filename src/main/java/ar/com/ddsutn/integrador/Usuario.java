c4de4baceacddc5dc283c0306d6e797d8f3549a5
NicoLuis <nicolasaluis@hotmail.com>
2015-05-01 16:43:04 -0300

    modifique validez usuario y tests + comentarios

--- src/main/java/ar/com/ddsutn/integrador/Usuario.java
+++ src/main/java/ar/com/ddsutn/integrador/Usuario.java
@@ -14,22 +14,44 @@ public class Usuario {
 	private Collection <String> palabrasDisgustan;
 	private Collection <Condicion> condiciones;
 	
+	private String rutina;		// ------ es String?? ------ <----<----<----<---- Revisar
+	
+	/*
 	protected Double altura; //PARA LA IMPLEMENTACION DEL MOCK, DESPUES CAMBIAR
 	protected Double peso;
+	(Le tienen que explicar a nico luis como es el tema del mock)
+	*/
+	
+	private Double altura;
+	private Double peso;
 	
 	public Usuario()
 	{super();}
 	
-	public Usuario (String nombre,String sexo, LocalDate fechaNacimiento, Double altura, Double peso,Collection<String> preferenciasAlimentacias,Collection<String>palabrasDisgustan, Collection<Condicion>condiciones){
+	public Usuario (String nombre, Double peso, Double altura, LocalDate fechaNacimiento, String sexo, Collection<Condicion>condiciones){
 		this.nombre = nombre;
-		this.sexo = sexo;
-		this.fechaNacimiento = fechaNacimiento;
-		this.altura = altura;
 		this.peso = peso;
-		this.preferenciasAlimenticias = preferenciasAlimentacias;
-		this.palabrasDisgustan = palabrasDisgustan;
+		this.altura = altura;
+		this.fechaNacimiento = fechaNacimiento;
+		this.sexo = sexo;
 		this.condiciones = condiciones;
+		/* elimine a this.preferenciasAlimenticias = preferenciasAlimentacias del constructor
+		 elimine a this.palabrasDisgustan = palabrasDisgustan del constructor
+		 => demasiados campos (creo que condiciones y sexo tambien deberian setearse fuera del contructor y rutina dentro)
+		 Modifique los campos para que tengan el orden que se pide en el enunciado */
 	}
+	
+	public Double imc ()
+	 {
+	  Double cuadradoAltura = Math.pow(altura, 2);
+	  return peso/cuadradoAltura;
+	  /* hace falta hacer Double cuadradoAltura? no seria mejor:
+	   public Double imc () { return peso/Math.pow(altura, 2) } ?
+	   o es mejor la expresividad de cuadradoAltura? */
+	 }
+	
+	
+	/*	IMPLEMENTACION ANTERIOR POR LUCHO
 	
 	public boolean esValidoElUsuario()
 	{	//APLICAR LAMBDA this.condiciones.validarBasico(this);
@@ -65,15 +87,44 @@ public class Usuario {
 			
 		}
 	}  //VER DE HACER ESTOS METODOS POLIMORFICOS A FUTURO
+	*/
 	
-	public Double imc ()
-	 {
-	  Double cuadradoAltura = Math.pow(altura, 2);
-	  return peso/cuadradoAltura;
-	 }
+	//  NUEVA IMPLEMENTACION
+	public boolean esValidoPorNombre(){
+		return this.nombre.length() > 4;
+	}
+	
+	public boolean esValidoPorFecha(){
+		return LocalDate.now().isAfter(this.fechaNacimiento);
+	}
+	
+	/* esValidoPorNombre() y esValidoPorFecha() las hice solo por expresividad
+	pero se podria incluir directamente el valor de retorno dentro
+	del return de esValidoPorCampos() */
 
+	public boolean esValidoPorCampos() {
+		return (this.nombre != null && 
+				this.peso != null && 
+				this.altura != null && 
+				this.fechaNacimiento != null && 
+				this.rutina != null &&
+				esValidoPorNombre() &&
+				esValidoPorFecha() );
+	}
+	
+	public boolean esValido() {
+		return (esValidoPorCampos());
+		/* Para probar los test quedo asi, pero deberia ser algo como: 
+		 return (esValidoPorCampos() && esValidoPorCondicion());
+		 donde esValidoPorCondicion() iria en cada clase condicion (hipertenso, diabetico, etc) */
+	}
+	
+	
+	
+	
 	public Collection <String> getPreferenciasAlimenticias() {
-		return preferenciasAlimenticias;
+		return this.preferenciasAlimenticias;
+		// cambie return preferenciasAlimenticias por return this.preferenciasAlimenticias;
 	}
 
 	public void setPreferenciasAlimenticias(Collection <String> preferenciasAlimenticias) {
@@ -81,10 +132,19 @@ public class Usuario {
 	}
 
 	public Collection <String> getPalabrasDisgustan() {
-		return palabrasDisgustan;
+		return this.palabrasDisgustan;
+		// cambie return palabrasDisgustan por return this.palabrasDisgustan;
 	}
 
 	public void setPalabrasDisgustan(Collection <String> palabrasDisgustan) {
 		this.palabrasDisgustan = palabrasDisgustan;
+	}
+	
+	public String getRutina() {
+		return this.rutina;
+	}
+
+	public void setRutina(String rutina) {
+		this.rutina = rutina;
 	}
 }
--- src/test/java/ar/com/ddsutn/integrador/UsuarioTest.java
+++ src/test/java/ar/com/ddsutn/integrador/UsuarioTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class UsuarioTest {
 	
 	
@@ -13,14 +15,24 @@ public class UsuarioTest {
 	private Usuario nicoLuis;
 	
 	@Before
-	
+	/*	Nico Luis no usa mock no sabe que es, expliquenle giles!
 	public void setUp() {
 	lucho = new UsuarioMock(1.80 , 81.0);
 	fede = new UsuarioMock(1.20, 72.0);
 	nico = new UsuarioMock(2.00, 60.0);
 	nicoLuis = new UsuarioMock(1.70, 57.8);
-	
 	}
+	*/
+	
+	public void setUp() {
+		
+	lucho = new Usuario("lucho",  81.0, 1.80, null, null, null);
+	fede = new Usuario("fede", 72.0, 1.20, null, null, null);
+	nico = new Usuario("nico", 60.0, 2.00, null, null, null);
+	nicoLuis = new Usuario("nicoLuis", 57.8 , 1.70, LocalDate.of(1995,6,24) , "unaRutina", null);
+	nicoLuis.setRutina("unaRutina");
+	}
+	
 	
 	@Test
 	
@@ -44,5 +56,12 @@ public class UsuarioTest {
 	public void imcSeCalculaBienParaNicoLuis()
 	{
 		assertEquals ((Double)20.0 , nicoLuis.imc());
+	}
+	
+	@Test
+	public void esValido()
+	{
+		assertEquals ((boolean)true , nicoLuis.esValido());
+		assertEquals ((boolean)false , lucho.esValido());
 	}
 }
