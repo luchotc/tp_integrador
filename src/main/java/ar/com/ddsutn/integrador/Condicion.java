6e087ade90b6aefc2a4deebd435dc3d040e3e098
NicooRama <nicolaserama@gmail.com>
2015-05-04 18:21:40 -0300

    borrado condicion de paquete principal

--- src/main/java/ar/com/ddsutn/integrador/Condicion.java
+++
@@ -1,9 +0,0 @@
-package ar.com.ddsutn.integrador;
-
-public interface Condicion {
-	
-	public boolean esValidoPorCondicion(Usuario usuario);
-	public boolean lograSubsanar(Usuario usuario);
-	public boolean esAdecuada(Usuario usuario, Receta receta);
-
-}
