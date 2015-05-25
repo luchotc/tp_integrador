74980e486c4c24f332a7ab5bd5a98f7b431fb8de
NicooRama <nicolaserama@gmail.com>
2015-05-25 18:58:17 -0300

    comparator alfabetico

--- src/main/java/ar/com/ddsutn/resultados/ResultadoAlfabetico.java
+++ src/main/java/ar/com/ddsutn/resultados/ResultadoAlfabetico.java
@@ -19,14 +19,12 @@ public class ResultadoAlfabetico implements Resultado  {
 		return recetas.stream().sorted((x,y)-> x.getNombre().compareTo(y.getNombre())).collect(Collectors.toList());
 	}
 	
-	@SuppressWarnings("hiding")
-	class comparadorAlfabetico <Receta> implements Comparator<Receta>
+	class comparadorAlfabetico implements Comparator<Receta>
 	{
 		@Override
 		public int compare(Receta r1, Receta r2) {
-			return ((ar.com.ddsutn.integrador.Receta) r1).getNombre().compareTo(((ar.com.ddsutn.integrador.Receta) r2).getNombre());
+			return (r1.getNombre().compareTo(r2.getNombre()));
 		}
-
 	}
 
 }
