362e14be5858a6878cad46cdb3d0622f307e64ac
FRodriguez18 <fede.18_87@hotmail.com>
2015-06-05 14:51:23 -0300

    Modificaciones sobre RepoUsuarios

--- src/main/java/ar/com/ddsutn/integrador/RepoUsuarios.java
+++ src/main/java/ar/com/ddsutn/integrador/RepoUsuarios.java
@@ -2,6 +2,7 @@ package ar.com.ddsutn.integrador;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 import java.util.stream.*;
 
 public class RepoUsuarios {
@@ -19,16 +20,18 @@ public class RepoUsuarios {
 		
 	}
 	
-	/*public Usuario get(Usuario usuario)//Debería encontrar el primero que cumpla con el nombre ese, pero no estaría andando
+	/*public Optional<Usuario> get(Usuario usuario)
 	{
-		Usuario usuarioRetorno = usuarios.stream().findFirst(u -> (u.getNombre()).equals(usuario.getNombre()));
+		Optional<Usuario> usuarioRetorno = usuarios.stream().filter(u -> (u.getNombre()).equals(usuario.getNombre())).findFirst();
 		return usuarioRetorno;
 	}*/
+	//No sé si anda esto, pero con tipo Usuario no anda el findFirst. Puede que lo esté pensando mal, pero es lo que se me ocurrió.
+	//Capaz se tiene que usar otra cosa, pero findFirst devuelve un Optional (Optional<T> devuelve un Null si no encuentra nada, y el objeto de tipo T si encuentra).
 	
 	public Collection<Usuario> list (Usuario usuario)
 	{
 		Collection<Usuario> retorno = new ArrayList<Usuario>();
-		retorno = usuarios.stream().filter(u -> (u.getNombre()).equals(usuario.getNombre())).collect(Collectors.toList()); //Filtra por nombre, buscando los que cumplen
+		retorno = usuarios.stream().filter(u -> (u.getNombre()).contains(usuario.getNombre())).collect(Collectors.toList()); //Filtra por nombre, buscando los que cumplen
 		/*if(tieneCondPreexistentes)
 		{
 			retorno = retorno.stream().filter(u-> u.cumpleCondPreexistentes(condPreexistentes));
